8cc818e717b7128ecae899f7ee6f1032838c2e00
Draemiz <draemizgx1@gmail.com>
2026-06-18 22:23:01 -0500

    Inicializa proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <header className="hero">
+        <div className="overlay">
+
+          <h1>
+            BIENVENIDO A
+            <br />
+            LOS MISTERIOS
+            <br />
+            DE GRAVITY FALLS
+          </h1>
+
+          <p>
+            Descubre secretos, conoce personajes
+            y explora los misterios del extraño
+            mundo de Gravity Falls.
+          </p>
+
+          <button>
+            Explorar Personajes
+          </button>
+
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
