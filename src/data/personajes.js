e9fd894857d6738675f24f14e3f93e8049444091
Draemiz <draemizgx1@gmail.com>
2026-06-20 03:08:38 -0500

    Se agrgo personajes, tranciciones, todo se muestra yfuncionaen la app

--- src/data/personajes.js
+++ src/data/personajes.js
@@ -1,205 +1,409 @@
 export const personajesData = [
-{
-      id: 1,
-      nombre: "Dipper Pines",
-      imagen: "https://i.pinimg.com/736x/61/dd/08/61dd0881c0b7ca8d41589d230852c043.jpg",
-      frase: "Los misterios están en todas partes.",
-      episodio: "Tourist Trapped",
+    {
+        id: 1,
+        nombre: "Dipper Pines",
+        imagen: "https://i.pinimg.com/736x/61/dd/08/61dd0881c0b7ca8d41589d230852c043.jpg",
+        frase: "Los misterios están en todas partes.",
+        episodio: "Tourist Trapped",
 
-      edad: 12,
-      rol: "Aventurero",
-      ubicacion: "Gravity Falls, Oregon",
+        edad: 12,
+        rol: "Aventurero",
+        ubicacion: "Gravity Falls, Oregon",
 
-      descripcion:
-        "Dipper es un chico inteligente y curioso que pasa el verano en Gravity Falls junto a su hermana gemela Mabel. Siempre busca resolver misterios y descubrir la verdad detrás de los fenómenos extraños del pueblo.",
+        descripcion:
+            "Dipper es un chico inteligente y curioso que pasa el verano en Gravity Falls junto a su hermana gemela Mabel. Siempre busca resolver misterios y descubrir la verdad detrás de los fenómenos extraños del pueblo.",
 
-      notas: [
-        "Muy curioso",
-        "Confía en Mabel",
-        "Ama los misterios"
-      ],
+        notas: [
+            "Muy curioso",
+            "Confía en Mabel",
+            "Ama los misterios"
+        ],
 
-      habilidades: [
-        "Curioso, ",
-        "Valiente, ",
-        "Inteligente, ",
-        "Leal."
-      ],
+        habilidades: [
+            "Curioso, ",
+            "Valiente, ",
+            "Inteligente, ",
+            "Leal."
+        ],
 
-      favoritos: [
-        "Resolver misterios, ",
-        "Leer, ",
-        "Aventuras."
-      ]
+        favoritos: [
+            "Resolver misterios, ",
+            "Leer, ",
+            "Aventuras."
+        ]
     },
 
     {
-      id: 2,
-      nombre: "Mabel Pines",
-      imagen: "https://static.wikia.nocookie.net/gravityfalls/images/a/a0/Mabel_pines_perfil.png/revision/latest?cb=20170824171732&path-prefix=es",
-      frase: "La vida es mejor con brillantina.",
-      episodio: "Tourist Trapped",
+        id: 2,
+        nombre: "Mabel Pines",
+        imagen: "https://static.wikia.nocookie.net/gravityfalls/images/a/a0/Mabel_pines_perfil.png/revision/latest?cb=20170824171732&path-prefix=es",
+        frase: "La vida es mejor con brillantina.",
+        episodio: "Tourist Trapped",
 
-      edad: 12,
-      rol: "Optimista",
-      ubicacion: "Gravity Falls, Oregon",
+        edad: 12,
+        rol: "Optimista",
+        ubicacion: "Gravity Falls, Oregon",
 
-      descripcion:
-        "Mabel es alegre, creativa y extrovertida. Siempre ve el lado positivo de las cosas y ayuda a sus amigos con entusiasmo.",
+        descripcion:
+            "Mabel es alegre, creativa y extrovertida. Siempre ve el lado positivo de las cosas y ayuda a sus amigos con entusiasmo.",
 
-      notas: [
-        "Ama los suéteres",
-        "Le gustan los cerditos",
-        "Muy optimista"
-      ],
+        notas: [
+            "Ama los suéteres",
+            "Le gustan los cerditos",
+            "Muy optimista"
+        ],
 
-      habilidades: [
-        "Creativa, ",
-        "Amable, ",
-        "Divertida, ",
-        "Valiente."
-      ],
+        habilidades: [
+            "Creativa, ",
+            "Amable, ",
+            "Divertida, ",
+            "Valiente."
+        ],
 
-      favoritos: [
-        "Brillantina",
-        "Suéteres",
-        "Waddles"
-      ]
+        favoritos: [
+            "Brillantina",
+            "Suéteres",
+            "Waddles"
+        ]
     },
 
     {
-      id: 3,
-      nombre: "Stan Pines",
-      imagen: "https://static.wikia.nocookie.net/gravityfalls/images/9/92/S1e16_something_about_you.png/revision/latest?cb=20130530141339",
-      frase: "Todo tiene un precio.",
-      episodio: "Headhunters",
+        id: 3,
+        nombre: "Stan Pines",
+        imagen: "https://static.wikia.nocookie.net/gravityfalls/images/9/92/S1e16_something_about_you.png/revision/latest?cb=20130530141339",
+        frase: "Todo tiene un precio.",
+        episodio: "Headhunters",
 
-      edad: 58,
-      rol: "Dueño del Mystery Shack",
-      ubicacion: "Gravity Falls, Oregon",
+        edad: 58,
+        rol: "Dueño del Mystery Shack",
+        ubicacion: "Gravity Falls, Oregon",
 
-      descripcion:
-        "Stan dirige el Mystery Shack. Aunque parece interesado únicamente en ganar dinero, se preocupa profundamente por su familia.",
+        descripcion:
+            "Stan dirige el Mystery Shack. Aunque parece interesado únicamente en ganar dinero, se preocupa profundamente por su familia.",
 
-      notas: [
-        "Gran comerciante",
-        "Protector",
-        "Misterioso"
-      ],
+        notas: [
+            "Gran comerciante",
+            "Protector",
+            "Misterioso"
+        ],
 
-      habilidades: [
-        "Astuto, ",
-        "Persistente, ",
-        "Líder, ",
-        "Valiente."
-      ],
+        habilidades: [
+            "Astuto, ",
+            "Persistente, ",
+            "Líder, ",
+            "Valiente."
+        ],
 
-      favoritos: [
-        "Dinero",
-        "Familia",
-        "Mystery Shack"
-      ]
+        favoritos: [
+            "Dinero",
+            "Familia",
+            "Mystery Shack"
+        ]
     },
 
     {
-      id: 4,
-      nombre: "Ford Pines",
-      imagen: "https://i.redd.it/if8zmslgg46e1.jpeg",
-      frase: "El conocimiento es poder.",
-      episodio: "The Author",
+        id: 4,
+        nombre: "Ford Pines",
+        imagen: "https://i.redd.it/if8zmslgg46e1.jpeg",
+        frase: "El conocimiento es poder.",
+        episodio: "The Author",
 
-      edad: 60,
-      rol: "Científico",
-      ubicacion: "Gravity Falls, Oregon",
+        edad: 60,
+        rol: "Científico",
+        ubicacion: "Gravity Falls, Oregon",
 
-      descripcion:
-        "Ford es el autor de los diarios y uno de los científicos más brillantes de Gravity Falls. Dedicó su vida a estudiar lo paranormal.",
+        descripcion:
+            "Ford es el autor de los diarios y uno de los científicos más brillantes de Gravity Falls. Dedicó su vida a estudiar lo paranormal.",
 
-      notas: [
-        "Escribió los diarios",
-        "Genio científico",
-        "Investigador paranormal"
-      ],
+        notas: [
+            "Escribió los diarios",
+            "Genio científico",
+            "Investigador paranormal"
+        ],
 
-      habilidades: [
-        "Inteligente, ",
-        "Analítico, ",
-        "Inventor, ",
-        "Valiente."
-      ],
+        habilidades: [
+            "Inteligente, ",
+            "Analítico, ",
+            "Inventor, ",
+            "Valiente."
+        ],
 
-      favoritos: [
-        "Investigación",
-        "Ciencia",
-        "Descubrimientos"
-      ]
+        favoritos: [
+            "Investigación",
+            "Ciencia",
+            "Descubrimientos"
+        ]
     },
 
     {
-      id: 5,
-      nombre: "Soos Ramirez",
-      imagen: "https://static.wikia.nocookie.net/gravityfalls/images/8/81/S1e1_soos_touching_hat.png/revision/latest/scale-to-width-down/985?cb=20120621195632",
-      frase: "¡Eso estuvo increíble, dude!",
-      episodio: "Tourist Trapped",
+        id: 5,
+        nombre: "Soos Ramirez",
+        imagen: "https://static.wikia.nocookie.net/gravityfalls/images/8/81/S1e1_soos_touching_hat.png/revision/latest/scale-to-width-down/985?cb=20120621195632",
+        frase: "¡Eso estuvo increíble, dude!",
+        episodio: "Tourist Trapped",
 
-      edad: 22,
-      rol: "Manitas del Mystery Shack",
-      ubicacion: "Gravity Falls, Oregon",
+        edad: 22,
+        rol: "Manitas del Mystery Shack",
+        ubicacion: "Gravity Falls, Oregon",
 
-      descripcion:
-        "Soos es amable, divertido y siempre está dispuesto a ayudar. Es considerado uno de los mejores amigos de Dipper y Mabel.",
+        descripcion:
+            "Soos es amable, divertido y siempre está dispuesto a ayudar. Es considerado uno de los mejores amigos de Dipper y Mabel.",
 
-      notas: [
-        "Muy amigable",
-        "Leal",
-        "Excelente mecánico"
-      ],
+        notas: [
+            "Muy amigable",
+            "Leal",
+            "Excelente mecánico"
+        ],
 
-      habilidades: [
-        "Mecánica, ",
-        "Lealtad, ",
-        "Humor, ",
-        "Creatividad."
-      ],
+        habilidades: [
+            "Mecánica, ",
+            "Lealtad, ",
+            "Humor, ",
+            "Creatividad."
+        ],
 
-      favoritos: [
-        "Videojuegos",
-        "Reparar cosas",
-        "Hamburguesas"
-      ]
+        favoritos: [
+            "Videojuegos",
+            "Reparar cosas",
+            "Hamburguesas"
+        ]
     },
 
     {
-      id: 6,
-      nombre: "Wendy Corduroy",
-      imagen: "https://i.pinimg.com/236x/86/a9/47/86a9479b15944b2a8b4568d78f81d2b0.jpg",
-      frase: "Relájate, todo estará bien.",
-      episodio: "The Inconveniencing",
+        id: 6,
+        nombre: "Wendy Corduroy",
+        imagen: "https://i.pinimg.com/236x/86/a9/47/86a9479b15944b2a8b4568d78f81d2b0.jpg",
+        frase: "Relájate, todo estará bien.",
+        episodio: "The Inconveniencing",
 
-      edad: 15,
-      rol: "Empleado del Mystery Shack",
-      ubicacion: "Gravity Falls, Oregon",
+        edad: 15,
+        rol: "Empleado del Mystery Shack",
+        ubicacion: "Gravity Falls, Oregon",
 
-      descripcion:
-        "Wendy es relajada, aventurera y valiente. Trabaja en el Mystery Shack y se convierte en una gran amiga de Dipper.",
+        descripcion:
+            "Wendy es relajada, aventurera y valiente. Trabaja en el Mystery Shack y se convierte en una gran amiga de Dipper.",
 
-      notas: [
-        "Excelente tiradora",
-        "Aventurera",
-        "Muy relajada"
-      ],
+        notas: [
+            "Excelente tiradora",
+            "Aventurera",
+            "Muy relajada"
+        ],
 
-      habilidades: [
-        "Valentía, ",
-        "Supervivencia, ",
-        "Liderazgo, ",
-        "Agilidad."
-      ],
+        habilidades: [
+            "Valentía, ",
+            "Supervivencia, ",
+            "Liderazgo, ",
+            "Agilidad."
+        ],
 
-      favoritos: [
-        "Bosques",
-        "Aventuras",
-        "Amigos"
-      ]
+        favoritos: [
+            "Bosques",
+            "Aventuras",
+            "Amigos"
+        ]
+    },
+
+    {
+        id: 7,
+        nombre: "Bill Cipher",
+        imagen: "https://static.wikia.nocookie.net/disney/images/4/41/Profile_-_Bill_Cipher.png/revision/latest?cb=20190318132904",
+        frase: "El universo es un holograma.",
+        episodio: "Dreamscaperers",
+
+        edad: "Desconocida",
+        rol: "Villano Interdimensional",
+        ubicacion: "Mindscape",
+
+        descripcion:
+            "Bill Cipher es una entidad demoníaca de otra dimensión con enormes poderes. Manipula los sueños y busca conquistar la realidad.",
+
+        notas: [
+            "Extremadamente peligroso",
+            "Manipulador",
+            "Enemigo de los Pines"
+        ],
+
+        habilidades: [
+            "Telepatía",
+            "Manipulación de la realidad",
+            "Control mental",
+            "Viaje dimensional"
+        ],
+
+        favoritos: [
+            "Caos",
+            "Acuerdos engañosos",
+            "Destrucción"
+        ]
+    },
+
+    {
+        id: 8,
+        nombre: "Pacifica Northwest",
+        imagen: "https://i.pinimg.com/474x/43/7a/fa/437afa004e7b84e258576012d4feeef5.jpg",
+        frase: "No soy quien solía ser.",
+        episodio: "Northwest Mansion Mystery",
+
+        edad: 12,
+        rol: "Heredera",
+        ubicacion: "Gravity Falls, Oregon",
+
+        descripcion:
+            "Pacifica proviene de una familia adinerada. Con el tiempo aprende a ser más humilde y se convierte en aliada de Dipper.",
+
+        notas: [
+            "Rica",
+            "Orgullosa",
+            "Valiente"
+        ],
+
+        habilidades: [
+            "Liderazgo",
+            "Determinación",
+            "Confianza",
+            "Elegancia"
+        ],
+
+        favoritos: [
+            "Moda",
+            "Caballos",
+            "Fiestas"
+        ]
+    },
+
+    {
+        id: 9,
+        nombre: "Gideon Gleeful",
+        imagen: "https://i.redd.it/nfnmg2iy81gf1.png",
+        frase: "¡Todo será mío!",
+        episodio: "The Hand That Rocks the Mabel",
+
+        edad: 9,
+        rol: "Antagonista",
+        ubicacion: "Tent of Telepathy",
+
+        descripcion:
+            "Gideon es un niño psíquico obsesionado con obtener poder y controlar Gravity Falls.",
+
+        notas: [
+            "Manipulador",
+            "Ambicioso",
+            "Celoso"
+        ],
+
+        habilidades: [
+            "Hipnosis",
+            "Persuasión",
+            "Actuación",
+            "Estrategia"
+        ],
+
+        favoritos: [
+            "Poder",
+            "Mabel",
+            "Diarios"
+        ]
+    },
+
+    {
+        id: 10,
+        nombre: "Pato",
+        imagen: "https://static.wikia.nocookie.net/disney/images/7/7d/Profile_-_Waddles.png/revision/latest/thumbnail/width/360/height/360?cb=20190319133843",
+        frase: "Oink!",
+        episodio: "The Time Traveler's Pig",
+
+        edad: 1,
+        rol: "Mascota",
+        ubicacion: "Mystery Shack",
+
+        descripcion:
+            "Waddles es el querido cerdito de Mabel y uno de los personajes más adorables de Gravity Falls.",
+
+        notas: [
+            "Mascota de Mabel",
+            "Muy adorable",
+            "Le encanta comer"
+        ],
+
+        habilidades: [
+            "Ternura",
+            "Compañerismo",
+            "Suerte",
+            "Carisma"
+        ],
+
+        favoritos: [
+            "Comida",
+            "Mabel",
+            "Dormir"
+        ]
+    },
+
+    {
+        id: 11,
+        nombre: "Robbie Valentino",
+        imagen: "https://i.pinimg.com/736x/b9/03/38/b9033899e86acfe88b0af168af683cf1.jpg",
+        frase: "Nada importa.",
+        episodio: "The Inconveniencing",
+
+        edad: 16,
+        rol: "Estudiante",
+        ubicacion: "Gravity Falls High School",
+
+        descripcion:
+            "Robbie es un adolescente gótico con actitud rebelde y novio ocasional de Wendy.",
+
+        notas: [
+            "Sarcasmo constante",
+            "Músico",
+            "Pesimista"
+        ],
+
+        habilidades: [
+            "Música",
+            "Actitud",
+            "Creatividad",
+            "Persistencia"
+        ],
+
+        favoritos: [
+            "Rock",
+            "Guitarra",
+            "Wendy"
+        ]
+    },
+
+    {
+        id: 12,
+        nombre: "Viejo McGucket",
+        imagen: "https://static.wikia.nocookie.net/disney/images/3/31/Profile_-_Old_Man_McGucket.png/revision/latest?cb=20190319133608",
+        frase: "¡Mi memoria no es la misma!",
+        episodio: "The Legend of the Gobblewonker",
+
+        edad: 70,
+        rol: "Inventor",
+        ubicacion: "Gravity Falls, Oregon",
+
+        descripcion:
+            "McGucket es un inventor brillante que perdió gran parte de su memoria tras experimentar con tecnología paranormal.",
+
+        notas: [
+            "Genio incomprendido",
+            "Excéntrico",
+            "Inventor"
+        ],
+
+        habilidades: [
+            "Ingeniería",
+            "Inventos",
+            "Mecánica",
+            "Creatividad"
+        ],
+
+        favoritos: [
+            "Chatarra",
+            "Inventar",
+            "Robots"
+        ]
     }
-  ];
+];
